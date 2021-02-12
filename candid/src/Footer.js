92eb15736c784d14da13029cb2f58f7b416e64af
sunnysunny77 <46119793+sunnysunny77@users.noreply.github.com>
2021-02-12 17:22:29 +0800

    up

--- candid/src/Footer.js
+++ candid/src/Footer.js
@@ -6,45 +6,48 @@ import { Link } from "react-router-dom";
 function Footer(props) {
   return (
     <footer>
-      <ul id="ul">
-        <li className="bl">
+      <ul >
+        <li className="bl lid">
           <Link to="/">
             <HomeIcon />
           </Link>
         </li>
-        <li >
+        <li className="lid" >
           <Link to={"/" + props.buttons[0]}>
             {ReactHtmlParser(props.buttons[0])}
           </Link>
         </li>
-        <li >
+        <li className="lid">
           <Link to={"/" + props.buttons[1]}>
             {ReactHtmlParser(props.buttons[1])}
           </Link>
         </li>
-        <li >
+        <li  className="lid">
           <Link to={"/" + props.buttons[2]}>
             {ReactHtmlParser(props.buttons[2])}
           </Link>
         </li>
-        <li>
+        <li className="lid">
           <Link to={"/" + props.buttons[3]}>
             {ReactHtmlParser(props.buttons[3])}
           </Link>
         </li>
-        <li className="bl">
+        <li className="bl" id="imh">
+          <img
+          src="https://candid.s3-ap-southeast-2.amazonaws.com/foot.png"
+          alt="Smiley face"
+          width="40"
+          height="40"
+        />
+        </li>  
+        <li className="bl lid">
           {ReactHtmlParser(props.time)} / {ReactHtmlParser(props.day)}
         </li>
-        <li className="bl">
+        <li className="bl lid">
           <a href={"tel:" + props.ph}>{props.ph}</a>
         </li>
       </ul>
-      <img
-        src="https://candid.s3-ap-southeast-2.amazonaws.com/foot.png"
-        alt="Smiley face"
-        width="40"
-        height="40"
-      />
+      
     </footer>
   );
 }
