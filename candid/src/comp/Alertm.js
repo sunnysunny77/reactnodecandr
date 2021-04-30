558d1aa7c7d5b2c52ac5368ee9f43e08fb357234
sunnysunny77 <46119793+sunnysunny77@users.noreply.github.com>
2021-04-30 13:31:43 +0800

    up

--- candid/src/comp/Alertm.js
+++ candid/src/comp/Alertm.js
@@ -1,7 +1,7 @@
 import React from "react";
-
+import styles from "./Alertm.module.scss";
 const Alertm = (props) => {
-  return <div style={{ textAlign: "center" }}>{props.alert}</div>;
+  return <div style={{ textAlign: "center", fontFamily: styles.f4 }}>{props.alert}</div>;
 };
 
 export default Alertm;
