2bba779d54151fbbe50b9d5e02e1ace9dd6fcbcc
sunnysunny77 <46119793+sunnysunny77@users.noreply.github.com>
2021-06-17 11:16:41 +0800

    up

--- candid/src/comp/Home.js
+++ candid/src/comp/Home.js
@@ -316,16 +316,16 @@ class Home extends Component {
                 <img
                   src="https://candid.s3-ap-southeast-2.amazonaws.com/wel1.jpg"
                   alt="Slider image 1"
-                  width="1725"
-                  height="698"
+                  width="1920"
+                  height="778"
                 ></img>
               </div>
               <div>
                 <img
                   src="https://candid.s3-ap-southeast-2.amazonaws.com/wel2.jpg"
                   alt="Slider image 2"
-                  width="1725"
-                  height="698"
+                  width="1920"
+                  height="778"
                 ></img>
               </div>
             </Slider>
