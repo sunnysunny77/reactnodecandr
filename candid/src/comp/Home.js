33bb87df6c3ae106e61e1515e6b216ba5640e0ff
daniel <contractor@lateralaspect.com.au>
2024-03-21 19:55:50 +0800

    up

--- candid/src/comp/Home.js
+++ candid/src/comp/Home.js
@@ -38,7 +38,7 @@ const customStyles = {
   option: (provided, state) => ({
     color: styles.c13,
     fontSize: "medium",
-    margin: "4.5px 0",
+    marginTop: "4.5px",
     textAlign: "center",
     cursor: "pointer",
   }),
